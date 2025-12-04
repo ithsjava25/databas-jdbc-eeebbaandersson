e4c3d136676c80357f40426a2481659638b7590c
Ebba Andersson <eeebbaandersson@gmail.com>
2025-12-05 00:18:39 +0100

    Solved config issues and added field, constructor and methods to list-/get Moonmissions by id

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,17 +1,25 @@
 package com.example;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.time.LocalDateTime;
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
+
+    // Fält som lagrar argument
+    private final String[] arguments;
+
+    // Konstruktor för att tar emot argumenten
+    public Main(String[] arguments) {
+        this.arguments = arguments;
+    }
 
     static void main(String[] args) {
         if (isDevMode(args)) {
             DevDatabaseInitializer.start();
         }
-        new Main().run();
+        new Main(args).run();
     }
 
     public void run() {
@@ -27,10 +35,25 @@ public class Main {
         }
 
         try (Connection connection = DriverManager.getConnection(jdbcUrl, dbUser, dbPass)) {
+           System.out.println("SUCCESS: Database connection established.");
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
         //Todo: Starting point for your code
+
+        // Prompt för Username/password
+        //Validate them against account table (user + password)
+
+        //listMoonMissions(jdbcUrl, dbUser, dbPass);
+        getMoonMissionByID(jdbcUrl, dbUser, dbPass);
+
+
+
+//        if (arguments.length == 0) {
+//            System.out.println("Missing arguments.");
+//            return;
+//        }
+
     }
 
     /**
@@ -59,4 +82,67 @@ public class Main {
         }
         return (v == null || v.trim().isEmpty()) ? null : v.trim();
     }
+
+    public static void greetUser(){
+        System.out.println("--Welcome to this MoonMission Application!--");
+    }
+
+
+    private static void listMoonMissions(String jdbcUrl, String dbUser, String dbPass) {
+        String query = "select spacecraft from moon_mission";
+
+        try (Connection connection = DriverManager.getConnection(jdbcUrl, dbUser, dbPass);
+             PreparedStatement pstmt = connection.prepareStatement(query)
+        ) {
+
+            try (ResultSet result = pstmt.executeQuery()) {
+                while (result.next()) {
+                    System.out.println(result.getString("spacecraft"));
+                }
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void getMoonMissionByID(String jdbcUrl, String dbUser, String dbPass){
+        String query = "select mission_id, spacecraft from moon_mission";
+
+        try (Connection connection = DriverManager.getConnection(jdbcUrl, dbUser, dbPass);
+             PreparedStatement pstmt = connection.prepareStatement(query)
+        ) {
+            try (ResultSet result = pstmt.executeQuery()) {
+                while (result.next()) {
+                    int id = result.getInt("mission_id");
+                    String spacecraft = result.getString("spacecraft");
+                    System.out.println("mission_id: " + id + " spacecraft: " + spacecraft);
+                }
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+
+    }
 }
+
+    public static void countMoonMissionsByYear(){
+        System.out.println("--Count Moon Missions by Year--");
+
+    }
+
+    public static void createAccount(){
+        System.out.println("--Create Account--");
+    }
+
+    public static void updateAccount(){
+        System.out.println("--Create Account--");
+    }
+
+    public static void deleteAccount(){
+        System.out.println("--Delete Account--");
+    }
+ }
+
+
+
